b60d9843f0f664f2f28fe82195632d91d8ad34f2
marciovieira <marcio72vieira@hotmail.com>
2022-09-07 17:32:58 -0300

    Criação da pagina de Login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -15,6 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('template.templateadmin');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
 
 
