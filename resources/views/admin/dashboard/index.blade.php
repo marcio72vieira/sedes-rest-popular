aab749e1e848ea472ccfcd9c1b4815488549472d
marciovieira <marcio72vieira@hotmail.com>
2022-12-07 12:59:23 -0300

    Início implementação Gráficos

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -275,7 +275,7 @@
                 <!-- Card Header - Dropdown -->
                 <div
                     class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                    <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+                    <h6 class="m-0 font-weight-bold text-primary">Visão Geral de Compras</h6>
                     <div class="dropdown no-arrow">
                         <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
                             data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -292,8 +292,8 @@
                     </div>
                 </div>
                 <!-- Card Body -->
-                <div class="card-body">
-                    <div class="chart-area">
+                <div class="card-body"> {{-- <div class="chart-area"> <canvas id="myChart"></canvas></div> --}}
+                    <div>
                         <canvas id="myChart"></canvas>
                     </div>
                 </div>
@@ -323,7 +323,7 @@
                     </div>
                 </div>
                 <!-- Card Body -->
-                <div class="card-body">
+                <div class="card-body">{{-- <div class="chart-pie pt-4 pb-2"> <canvas id="myPieChart"></canvas> </div>  --}}
                     <div class="chart-pie pt-4 pb-2">
                         <canvas id="myPieChart"></canvas>
                     </div>
@@ -341,6 +341,42 @@
                 </div>
             </div>
         </div>
+
+
+        <!-- Area Chart -->
+        <div class="col-xl-8 col-lg-7">
+            <div class="card shadow mb-4">
+                <!-- Card Header - Dropdown -->
+                <div
+                    class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                    <h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
+                    <div class="dropdown no-arrow">
+                        <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink"
+                            data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
+                            aria-labelledby="dropdownMenuLink">
+                            <div class="dropdown-header">Dropdown Header:</div>
+                            <a class="dropdown-item" href="#">Action</a>
+                            <a class="dropdown-item" href="#">Another action</a>
+                            <div class="dropdown-divider"></div>
+                            <a class="dropdown-item" href="#">Something else here</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Card Body -->
+                <div class="card-body"> {{-- <div class="chart-area"> <canvas id="myChart"></canvas></div> --}}
+                    <div>
+                        <canvas id="myLineChart"></canvas>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
     </div>
     <!-- FIM Content Row GRÁFICOS -->
 
@@ -582,7 +618,62 @@
         });
 
 
+        /*
+        var ctx = document.getElementById('myLineChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
 
+            // The data for our dataset
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [{
+                    label: 'My First dataset',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                    fill: false
+                }]
+            },
+
+            // Configuration options go here
+            options: {
+
+            }
+        });
+        */
+
+        var ctx = document.getElementById('myLineChart').getContext('2d');
+        var chart = new Chart(ctx, {
+            // The type of chart we want to create
+            type: 'line',
+
+            // The data for our dataset
+            data: {
+                labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Ags', 'Sep', 'Out', 'Nov', 'Dec'],
+                datasets: [
+                    {
+                        label: 'Compra Normal',
+                        backgroundColor: 'rgb(255, 99, 132)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: [0, 10, 5, 2, 20, 30, 45],
+                        fill: false
+                    },
+                    {
+                        label: 'Compra AF',
+                        backgroundColor: 'rgb(0, 0, 0)',
+                        borderColor: 'rgb(255, 99, 132)',
+                        data: [0, 20, 10, 12, 0, 60, 85],
+                        fill: false
+                    }
+                ]
+            },
+
+            // Configuration options go here
+            options: {
+
+            }
+        });
 
     </script>
 
